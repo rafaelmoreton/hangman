5697dcdb77223dae1b38d66af480226e662140a2
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2021-12-01 10:02:12 -0300

    Extraction of the #start_menu method from Match#initialize. Extraction of the Match#select_words_from_dictionary method to the new HangmanDictionary module. Implemented the LoadedMatch child class. Refactored the detection of save, load and new inputs. Addition of the starting text explaining the game and commands.

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # hangman
 A game where the player is given a limited number of attempts to guess which secret word the computer has choosen
 
+The computer chooses a secret word between 5 and 12 letters (case insensitive) from a wordlist using module HangmanDictionary. The choice to allocate the word selection method into a module was due to the possibility of later expansion of the module, implenenting other methods for different rules on word selection and accounting for differently formatted wordlists.
 
-https://www.scrapmaker.com/view/twelve-dicts/5desk.txt
+A blank space is displayed for each letter of the secret word and the player is asked for a letter input. For every letter the player inputs the program checks if it's a valid letter and adds it to a list of tried letters. Besides, if it's present in the secret word the corresponding blank spaces are substituted by the letter. Otherwise one chance is deducted.
+
+If instead of a letter the player inputs one of the keywords 'save', 'load' or 'new' the corresponding action will take place. 'save' stores the Match object's current attributes into a hash, serialize it to YAML, save the file to the root directory and continues the game. 'load' and 'new' abort the current match and start a new one (parsing a string stored in a local YAML file into a data hash to have it's values setted as the corresponding new Match object attributes or generating fresh ones, respectively).
+
+Credits: https://www.scrapmaker.com/view/twelve-dicts/5desk.txt for the wordlist.
+
+
+Conclusion
+Making this project solidified the I/O, serialization and file manipulation concepts I've been studying. It also had the Module usefulness finnaly click to me in a pactical sense.
+
+This is an educational project from theodinproject.com.
+
+
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,27 +1,5 @@
-class Match
-    require 'yaml'
-    attr_reader :secret_word, :guessing_word
-
-    def initialize
-        @dictionary = select_words_from_dictionary("5desk_wordlist.txt")
-        @secret_word =  @dictionary.sample.split('')
-        @guessing_word = Array.new(@secret_word.length, "_")
-        @chances = 10
-        @tried_letters = []
-        
-        puts "Press '1' to start a new game or '2' to load a game."
-        game_choice = gets.chomp
-        if game_choice == "1"
-            self.play
-        elsif game_choice == "2"
-            self.load
-        else
-            puts "This isn't a valid option."
-        end
-
-    end
-
-    def select_words_from_dictionary(dictionary)
+module HangmanDictionary
+    def self.select_words_from_dictionary(dictionary)
         game_dictionary = []
         File.open(dictionary, 'r') do |content|
             until content.eof?
@@ -33,30 +11,53 @@ class Match
         game_dictionary
         end
     end
+end
+
+class Match
+    require 'yaml'
+    attr_reader :secret_word, :guessing_word
+
+    def initialize
+        @dictionary = HangmanDictionary.select_words_from_dictionary("5desk_wordlist.txt")
+        @secret_word =  @dictionary.sample.split('')
+        @guessing_word = Array.new(@secret_word.length, "_")
+        @chances = 10
+        @tried_letters = []
+    end
 
     def play
-        puts "\nThe new secret word has #{@secret_word.length} letters."
+        puts "\nThe secret word has #{@secret_word.length} letters."
         loop do
             puts "Tried letters: #{@tried_letters.join("-")}\nYou have #{@chances} remaining chances. Insert a letter."
             self.display
             letter_input = gets.chomp.downcase
             puts "\n"
-            self.check_letter(letter_input)
+            if letter_input == "save"
+                self.save
+            elsif letter_input == "load"
+                LoadedMatch.new
+                break
+            elsif letter_input == "new"
+                puts "Starting a new game.\n "
+                Match.new.play
+                break
+            else
+                self.check_letter(letter_input)
+            end
+
             if @secret_word == @guessing_word
                 self.display
                 puts "Victory!"
                 break
             elsif @chances == 0
-                puts "You lost.\n\nThe secret word was '#{@secret_word.join}'"
+                puts "You lost.\nThe secret word was '#{@secret_word.join}'"
                 break
             end
         end
     end
 
     def check_letter(letter)
-        if letter == "save"
-            self.save
-        elsif @tried_letters.any?(letter)
+        if @tried_letters.any?(letter)
             puts "You already tried '#{letter}'. Try another."
         elsif ("a".."z").to_a.none?(letter)
             puts "This isn't a valid letter. Try a single letter from 'a' to 'z'"
@@ -88,17 +89,48 @@ class Match
         File.open(savename, 'w') do |content|
             content.puts string
         end
+        puts "Game saved as '#{savename}'\n "
     end
-
-    def self.load
-        puts "What save file do you want to load?"
-        savename = gets.chomp
-        data = YAML.load(File.read(savename))
-        # self.new()
-    end
-
 end
 
-Match.new
+class LoadedMatch < Match
+    def initialize
+        @secret_word = nil
+        @guessing_word = nil
+        @chances = nil
+        @tried_letters = nil
+        self.loadgame
+    end
+
+    def loadgame
+        puts "What game do you want to load?"
+        filename = gets.chomp + ".yaml"
+        puts "Game '#{filename}' loaded\n "
+        loaded_data = YAML.load(File.read(filename))
+        @secret_word = loaded_data[:secret_word]
+        @guessing_word = loaded_data[:guessing_word]
+        @chances = loaded_data[:chances]
+        @tried_letters = loaded_data[:tried_letters]
+        self.play
+    end
+end
+
+def start_menu
+    puts "___________________________\n\nPress '1' to start a new game or '2' to load a game."
+    game_choice = gets.chomp
+    if game_choice == "1"
+        Match.new.play
+    elsif game_choice == "2"
+        LoadedMatch.new
+    else
+        puts "This isn't a valid option."
+    end
+end
+
+puts "\nLet's play Hangman!\n\nYou will have 10 chances to guess the letters of a secret word. If you wish to save the game, load a previoulsy saved one or start a new game, you can insert 'save', 'load' or 'new' respectively instead of a letter during the game.\n\nGood luck!"
+
+loop do
+    start_menu
+end
 
 
